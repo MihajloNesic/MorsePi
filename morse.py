c8395bc5ee3b389c051b982f14f0afe44e01dad7
Mihajlo Nešić <MihajloNesic@users.noreply.github.com>
2016-08-02 10:43:02 +0200

    Add files via upload

---
+++ morse.py
@@ -0,0 +1,85 @@
+import time
+import pifacedigitalio as p
+
+pf = p.PiFaceDigital()
+
+led=0
+
+CODE = {' ': ' ',
+        "'": '.----.',
+        '(': '-.--.-',
+        ')': '-.--.-',
+        ',': '--..--',
+        '-': '-....-',
+        '.': '.-.-.-',
+        '/': '-..-.',
+        '0': '-----',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        ':': '---...',
+        ';': '-.-.-.',
+        '?': '..--..',
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '_': '..--.-'}
+		
+def dot():
+	pf.leds[led].turn_on()
+	time.sleep(0.2)
+	pf.leds[led].turn_off()
+	time.sleep(0.2)
+
+def dash()
+	pf.leds[led].turn_on()
+	time.sleep(0.5)
+	pf.leds[led].turn_off()
+	time.sleep(0.5)
+	
+while True:
+	inp = input('What would you like to send? ')
+	
+	for ch in inp:
+		print(CODE[ch.upper()])
+	
+	time.sleep(0.2)
+	
+	for letter in inp:
+			for symbol in CODE[letter.upper()]:
+				if symbol == '-':
+					dash()
+				elif symbol == '.':
+					dot()
+				else:
+					time.sleep(0.5)
+			time.sleep(0.5)
